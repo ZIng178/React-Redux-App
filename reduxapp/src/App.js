6473f53a1011e934e681f1be3ddbb77f4114c0d0
Wangdi Tenzing <wangdi457@gmail.com>
2020-02-06 17:40:25 -0500

    Created a store , set up the reducer , created actions , added extensions for redux devtools

---
+++ reduxapp/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Actions from "./actions"
+import './App.css';
+import {getData} from "./actions"
+
+function App() {
+
+
+
+  return (
+    <div className="App">
+      <h1> Async Redux</h1>
+
+        
+    </div>
+  );
+}
+
+export default App;
